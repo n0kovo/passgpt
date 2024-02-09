234d49b3aa72d69e2e69363183d7e3030f379921
n0kovo <n0kovo@riseup.net>
2024-02-09 19:23:01 +0100

    Update lllamapass.py

--- src/lllamapass.py
+++ src/lllamapass.py
@@ -4,7 +4,8 @@ sys.path.append("../code")
 import os
 import argparse
 import torch
-from transformers import AutoModelForCausalLM, AutoConfig, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoConfig, RobertaTokenizer
+from tokenizers import ByteLevelBPETokenizer
 from datasets import load_dataset
 from pathlib import Path
 
@@ -22,6 +23,7 @@ from utils import *  # Assuming this includes necessary utilities like PasswordD
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_path", help="path to yaml config", type=str, required=True)
+    parser.add_argument("--subsample", help="subsample the dataset to this number of entries", type=int, default=-1)
     args = parser.parse_args()
 
     with open(args.config_path) as f:
@@ -41,14 +43,45 @@ if __name__ == "__main__":
     TOKENIZER_MAX_LEN = args.maxchars + 2
 
     print("===> Loading tokenizer")
-    tokenizer = AutoTokenizer.from_pretrained(args.tokenizer_path, model_max_length=TOKENIZER_MAX_LEN, use_fast=True)
 
+    tokenizer = RobertaTokenizer.from_pretrained(
+        '/drive/MyDrive/passgpt_llama2/tokenizer/byte_bpe_tokenizer_98/vocab.json',
+        '/drive/MyDrive/passgpt_llama2/tokenizer/byte_bpe_tokenizer_98/merges.txt',
+)
+
+    # Define dataloader
     print("===> Loading data")
-    # Data loading remains the same
 
-    data_files = {'train': [args.train_data_path]}
+    def preprocess_function(entries):
+        """
+        This function tokenizes a list of passwords. It appends the end of password token to each of them before processing.
+        """
+        to_tokenize = ['<s>' + p[:args.maxchars] +'</s>' for p in entries['text']]
+        return tokenizer(to_tokenize,
+                         truncation=True,
+                         padding="max_length",
+                         max_length=TOKENIZER_MAX_LEN,
+                         add_special_tokens=False,
+                         return_special_tokens_mask=False)
+
+
+    data_files = {'train': ["/drive/MyDrive/passgpt_llama2/hashmob.net_2024-02-05.user.found"]}
     dataset = load_dataset('text', data_files=data_files)
-    # Remainder of data processing stays the same
+    print("Dataset loaded with {} entries".format(len(dataset["train"])))
+
+    if args.subsample > 0:
+        print("Subsampling dataset to {} random entries".format(args.subsample))
+        dataset['train'] = dataset['train'].select([i for i in range(args.subsample)])
+
+
+    # Process data
+    print("===> Processing data")
+    tokenized_datasets = dataset.map(preprocess_function, batched=True, remove_columns=dataset["train"].column_names)
+    tokenized_datasets = tokenized_datasets.shuffle(seed=args.seed)
+
+    # Format data
+    tokenized_datasets.set_format(type="torch")
+
 
     print("===> Initializing model")
     model = AutoModelForCausalLM.from_pretrained("NousResearch/Llama-2-7b-hf")
@@ -57,7 +90,17 @@ if __name__ == "__main__":
 
     # Training setup
     from transformers import Trainer, TrainingArguments
-    # TrainingArguments setup remains largely the same, with adjustments for LLaMA as needed
+
+    print("===> Preparing training")
+    # Define the data collator. In charge of hiding tokens to be predicted.
+    data_collator = PasswordDataCollator(
+        tokenizer=tokenizer, mlm=False
+    )
+
+    train_args = TrainingArguments(
+            **training_args
+        )
+
 
     trainer = Trainer(
         model=model,
@@ -67,6 +110,18 @@ if __name__ == "__main__":
     )
 
     # Launch training
+    print("===> Launching training")
+    start = time.time()
     trainer.train()
+    end = time.time()
 
-    # Post-training handling remains the same
+    print("===> Training completed after {}. Storing last version.".format(str(timedelta(seconds=end-start))))
+    model.save_pretrained(os.path.join("/drive/MyDrive/passgpt_llama2/output", "last"))
+
+    # Comment out next lines if you want to keep several checkpoints.
+    print("===> Deleting previous checkpoints")
+    checkpoints = [i for i in os.listdir("/drive/MyDrive/passgpt_llama2/output") if i.startswith("checkpoint")]
+    for c in checkpoints:
+        shutil.rmtree(os.path.join("/drive/MyDrive/passgpt_llama2/output", c))
+
+    print("===> Training finished succesfully :)")
